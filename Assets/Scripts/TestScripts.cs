bc87a657d60ed3e3ad656951adca1b93db9bc83d
AlekseyBraksh <zxc101@mail.ru>
2020-06-20 15:08:41 +0300

    Start

---
+++ Assets/Scripts/TestScripts.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class TestScripts : SerializedMonoBehaviour
+{
+    public Dictionary<string, int> list;
+
+    void Start()
+    {
+        foreach(var pair in list)
+        {
+            Debug.Log(pair.Key + ": " + pair.Value);
+        }
+    }
+}
